1421317e9e3e4d6f8d140a50f546ef4e2aab46b4
Benjamin Myers <bnjmn.myers@gmail.com>
2025-01-04 10:56:29 -0500

    Added kwya logo

---
+++ src/components/SVGs/kwyaLogo.js
@@ -0,0 +1,40 @@
+import React from "react"
+
+export default function KWYALogo() {
+  return (
+    <svg
+      width="247px"
+      height="100px"
+      viewBox="0 0 247 100"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <g
+        id="Page-1"
+        stroke="none"
+        stroke-width="1"
+        fill="#000000"
+        fillRule="evenodd"
+        fontFamily="HelveticaNeue, Helvetica Neue"
+        fontSize="86"
+        fontWeight="normal"
+      >
+        <g
+          id="logo"
+          transform="translate(-48.59, -122)"
+          fill="#1C0001"
+          fillRule="nonzero"
+        >
+          <g id="#kwya" transform="translate(48.59, 122)">
+            <text>
+              <tspan x="-1.42108547e-14" y="82">
+                #kwya
+              </tspan>
+            </text>
+          </g>
+        </g>
+      </g>
+    </svg>
+  )
+}
